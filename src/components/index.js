2b5744cca6890aae3c11485f2308bb87562adf5d
Erudite885 <erudite885@gmail.com>
2022-10-18 03:56:51 +0100

    helper components imported

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,14 @@ import Header from './Header.jsx';
 import Hero from "./Hero.jsx";
 import Subscribe from "./Subscribe.jsx";
 import Top from "./Top.jsx";
+import Heading from "./Heading.jsx";
+import Counter from "./Counter.jsx";
 
 export {
     Header,
     Hero,
     Subscribe,
-    Top
+    Top,
+    Heading,
+    Counter
 };
