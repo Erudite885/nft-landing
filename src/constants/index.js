4562df678f07fb42c78113add22139e98c6c4617
Erudite885 <erudite885@gmail.com>
2022-10-19 07:53:56 +0100

    Constants folder added

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+import {main, one, two, three, four, five, six, seven, eight} from "../assets/index"
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "about us",
+  },
+  {
+    id: "explore",
+    title: "explore",
+  },
+  //  {
+  //     id: "clients",
+  //     title:"Clients",
+  // },
+];
