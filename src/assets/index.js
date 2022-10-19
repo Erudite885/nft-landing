5901736a9ffbef9b38fe15343d5e8a3b4c83c479
Erudite885 <erudite885@gmail.com>
2022-10-19 08:10:28 +0100

    assets folder

---
+++ src/assets/index.js
@@ -0,0 +1,34 @@
+import one from "./1.png";
+import two from "./2.png";
+import three from "./3.png";
+import four from "./4.png";
+import five from "./5.jpg";
+import six from "./6.jpg";
+import seven from "./7.jpg";
+import eight from "./8.jpg";
+import main from "./main.png";
+import facebook from "./facebook.svg";
+import google from "./google.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import apple from "./apple.svg";
+
+export {
+    one,
+    two, 
+    three, 
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    main, 
+    facebook,
+    google,
+    instagram,
+    linkedin,
+    twitter,
+    apple
+
+};
