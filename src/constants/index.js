2a9ebc5760f4318e9f17844034ee8bd2b8d5efac
Erudite885 <erudite885@gmail.com>
2022-10-19 08:12:02 +0100

    footer links and socials added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import {main, one, two, three, four, five, six, seven, eight} from "../assets/index"
+import {main, one, two, three, four, five, six, seven, eight, facebook, instagram, linkedin, twitter} from "../assets/index"
 
 export const navLinks = [
   {
@@ -13,8 +13,97 @@ export const navLinks = [
     id: "explore",
     title: "explore",
   },
-  //  {
-  //     id: "clients",
-  //     title:"Clients",
-  // },
+];
+
+
+export const footerLinks = [
+  {
+    key: 1,
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.thecoffeeroasters.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.thecoffeeroasters.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.thecoffeeroasters.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.thecoffeeroasters.com/explore/",
+      },
+      {
+        name: "Terms & Service",
+        link: "https://www.thecoffeeroasters.com/terms-and-Service/",
+      },
+    ],
+  },
+  {
+    key: 2,
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.thecoffeeroasters.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.thecoffeeroasters.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.thecoffeeroasters.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.thecoffeeroasters.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.thecoffeeroasters.com/newsletters/",
+      },
+    ],
+  },
+  {
+    key: 3,
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.thecoffeeroasters.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.thecoffeeroasters.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
 ];
