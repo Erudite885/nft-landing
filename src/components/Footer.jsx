ff2437da835000b80740268145bf209bd1957257
Erudite885 <erudite885@gmail.com>
2022-10-19 08:23:59 +0100

    footer paragraph adjusted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,18 @@ import { footerLinks, socialMedia } from "../constants";
 // import { hotCoffee } from "../assets";
 
 const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+  <section
+    className='flex justify-center items-center sm:py-16 py-6 flex-col'
+  >
+    <div
+      className='flex justify-center items-start md:flex-row flex-col mb-8 w-full'
+    >
       <div className="flex-1 flex flex-col justify-start mr-10">
-       <p>NFT</p>
-        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-          Not just a drop-in get coffee service, but a new way to Sit, Relax and
-          enjoy that tailor-made cup of coffee in a new way.
+        <p>NFT</p>
+        <p
+          className='font-poppins font-normal text-[18px] leading-[30.8px] mt-4 max-w-[310px]'
+        >
+          write something about nfts here....
         </p>
       </div>
 
@@ -42,8 +47,8 @@ const Footer = () => (
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[#3f3e45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Non Fungible Tokens. 
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] ">
+        Non Fungible Tokens.
       </p>
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
