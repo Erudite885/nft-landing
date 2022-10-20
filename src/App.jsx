792e332af4c196bdf29e5f630cec3cdad0109440
Erudite885 <erudite885@gmail.com>
2022-10-20 01:28:04 +0100

    footer imported and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Header, Hero, Top, Subscribe} from './components/index'
+import {Header, Hero, Top, Subscribe, Footer} from './components/index'
 
 const App = () => {
   return (
-    <div>
+    <div className='container mx-auto sm:px-10 px-5 pt-10'>
       <Header/>
       <Hero/>
       <Top/>
       <Subscribe/>
+      <Footer/>
     </div>
   )
 }
