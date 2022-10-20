a7e4f0ea6801556cc4e5c63832ac1a1e7f63ba1d
Erudite885 <erudite885@gmail.com>
2022-10-20 01:26:21 +0100

    Subscribe section added

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Subscribe = () => {
   return (
-    <div>
-      <div>Subscribe</div>
-    </div>
+    <section id='subscribe'>
+      <header className="flex flex-col justify-center items-center mb-12 ">
+        <h2 className="font-bold tracking-wide text-xl uppercase">
+          Subscribe
+        </h2>
+        <hr className="w-24 h-1 mt-2 bg-pink-500 border-0 " />
+      </header>
+    </section>
   );
 };
 
